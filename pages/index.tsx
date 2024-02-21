36d6f33cb47d8a1cf8741bcf1dce0cd5a514eac7
Joshua B <desertflame3@gmail.com>
2024-02-21 12:56:12 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
       <div className="h-screen overflow-auto bg-[#18181C] text-[#D4D4D8]">
         <a
           className="absolute top-0 right-12 p-4 cursor-pointer"
-          href="https://twitter.com/mckaywrigley"
+          href="https://twitter.com/"
           target="_blank"
           rel="noreferrer"
         >
@@ -39,7 +39,7 @@ export default function Home() {
 
         <a
           className="absolute top-0 right-2 p-4 cursor-pointer"
-          href="https://github.com/mckaywrigley/clarity-ai"
+          href="https://github.com/therysin/clarity-ai"
           target="_blank"
           rel="noreferrer"
         >
